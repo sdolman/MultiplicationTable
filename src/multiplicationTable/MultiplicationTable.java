f23331684e5f3e78931127b9255d830c18251c8a
Seth Dolman <sdolman@users.noreply.github.com>
2017-01-17 16:33:37 -0500

    12 times table

---
+++ src/multiplicationTable/MultiplicationTable.java
@@ -0,0 +1,17 @@
+package multiplicationTable;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 12; i++) {
+			for(int m= 1; m <= 12; m++) {
+				System.out.print("" + i*m + " ");
+			}
+			System.out.println("");
+			
+		}
+
+	}
+
+}
