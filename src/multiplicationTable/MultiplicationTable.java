974f5b4d4435605b61243d2c39ab927ab76cb037
Seth Dolman <sdolman@users.noreply.github.com>
2017-01-17 19:45:10 -0500

    refactor magic number into constant

--- src/multiplicationTable/MultiplicationTable.java
+++ src/multiplicationTable/MultiplicationTable.java
@@ -2,14 +2,15 @@ package multiplicationTable;
 
 public class MultiplicationTable {
 
+	private static final int LIMIT = 12;
+
 	public static void main(String[] args) {
 		
-		for(int i = 1; i <= 12; i++) {
-			for(int m= 1; m <= 12; m++) {
+		for (int i = 1; i <= LIMIT; i++) {
+			for(int m= 1; m <= LIMIT; m++) {
 				System.out.print("" + i*m + " ");
 			}
 			System.out.println("");
-			
 		}
 
 	}
